45e6b243096fc55232e4654ed8700bc1730f3c03
yuhan <guoyuhan819@gmail.com>
2018-03-29 23:09:46 +0200

    finish it

--- mod/learn_joint_bpe_and_vocab.py
+++ mod/learn_joint_bpe_and_vocab.py
@@ -17,37 +17,45 @@ import argparse
 import tempfile
 from collections import Counter
 
-# local file
+# local module
 import learn_bpe
 import apply_bpe
 
 
 def create_parser():
+
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="learn BPE-based word segmentation")
+        description="learn and apply BPE-based word segmentation")
 
     parser.add_argument(
-        '--input', '-i', type=argparse.FileType('r'), required=True, nargs = '+',
-        metavar='PATH',
+        '--input', '-i', type=argparse.FileType('r', encoding='UTF-8'),
+        metavar='PATH', required=True, nargs = '+',
         help="Input texts (multiple allowed).")
+
     parser.add_argument(
-        '--output', '-o', type=argparse.FileType('w'), required=True,
-        metavar='PATH',
+        '--output', '-o', type=argparse.FileType('w', encoding='UTF-8'),
+        metavar='PATH', required=True,
         help="Output file for BPE codes.")
+
     parser.add_argument(
         '--symbols', '-s', type=int, default=10000,
         help="Create this many new symbols (each representing a character n-gram) (default: %(default)s))")
+
     parser.add_argument(
         '--separator', type=str, default='@@', metavar='STR',
         help="Separator between non-final subword units (default: '%(default)s'))")
+
     parser.add_argument(
-        '--write-vocabulary', type=argparse.FileType('w'), nargs = '+', default=None,
+        '--write-vocabulary', type=argparse.FileType('w', encoding='UTF-8'), 
+        nargs = '+', default=None,
         metavar='PATH', dest='vocab',
         help='Write to these vocabulary files after applying BPE. One per input text. Used for filtering in apply_bpe.py')
+
     parser.add_argument(
         '--min-frequency', type=int, default=2, metavar='FREQ',
         help='Stop if no symbol pair has frequency >= FREQ (default: %(default)s))')
+        
     parser.add_argument(
         '--verbose', '-v', action="store_true",
         help="verbose mode.")
@@ -55,29 +63,15 @@ def create_parser():
     return parser
 
 
-
 if __name__ == '__main__':
-
-    # python 2/3 compatibility
-    if sys.version_info < (3, 0):
-        sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
-        sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
-        sys.stdin = codecs.getreader('UTF-8')(sys.stdin)
-    else:
-        sys.stderr = codecs.getwriter('UTF-8')(sys.stderr.buffer)
-        sys.stdout = codecs.getwriter('UTF-8')(sys.stdout.buffer)
-        sys.stdin = codecs.getreader('UTF-8')(sys.stdin.buffer)
 
     parser = create_parser()
     args = parser.parse_args()
 
+    # equal input and vocab file
     if args.vocab and len(args.input) != len(args.vocab):
         sys.stderr.write('Error: number of input files and vocabulary files must match\n')
         sys.exit(1)
-
-    # read/write files as UTF-8
-    args.input = [codecs.open(f.name, encoding='UTF-8') for f in args.input]
-    args.vocab = [codecs.open(f.name, 'w', encoding='UTF-8') for f in args.vocab]
 
     # get combined vocabulary of all input texts
     full_vocab = Counter()
@@ -88,19 +82,19 @@ if __name__ == '__main__':
     vocab_list = ['{0} {1}'.format(key, freq) for (key, freq) in full_vocab.items()]
 
     # learn BPE on combined vocabulary
-    with codecs.open(args.output.name, 'w', encoding='UTF-8') as output:
-        learn_bpe.main(vocab_list, output, args.symbols, args.min_frequency, args.verbose, is_dict=True)
+    learn_bpe.main(vocab_list, args.output, args.symbols, args.min_frequency, args.verbose, is_dict=True)
 
-    with codecs.open(args.output.name, encoding='UTF-8') as codes:
+    with open(args.output.name, 'r', encoding='UTF-8') as codes:
         bpe = apply_bpe.BPE(codes, separator=args.separator)
 
     # apply BPE to each training corpus and get vocabulary
     for train_file, vocab_file in zip(args.input, args.vocab):
 
+        # temp file to save segmented data
         tmp = tempfile.NamedTemporaryFile(delete=False)
         tmp.close()
 
-        tmpout = codecs.open(tmp.name, 'w', encoding='UTF-8')
+        tmpout = open(tmp.name, 'w', encoding='UTF-8')
 
         train_file.seek(0)
         for line in train_file:
@@ -108,12 +102,14 @@ if __name__ == '__main__':
             tmpout.write('\n')
 
         tmpout.close()
-        tmpin = codecs.open(tmp.name, encoding='UTF-8')
+        tmpin = open(tmp.name, 'r', encoding='UTF-8')
 
+        # get vocab from segmented data
         vocab = learn_bpe.get_vocabulary(tmpin)
         tmpin.close()
         os.remove(tmp.name)
 
+        # vocab freq descending
         for key, freq in sorted(vocab.items(), key=lambda x: x[1], reverse=True):
             vocab_file.write("{0} {1}\n".format(key, freq))
         vocab_file.close()
